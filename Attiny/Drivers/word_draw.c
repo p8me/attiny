b3d0acd2f8315051346d08cb2799a3a1f75245a1
HARMONYBIG\Pyrus <pyrus.lamp@gmail.com>
2018-02-17 23:56:23 -0500

    Word draw stable based on math! Added possible characters

--- Attiny/Drivers/word_draw.c
+++ Attiny/Drivers/word_draw.c
@@ -15,6 +15,8 @@
 
 const uint8_t TONE_PIN[2] = {0, 1};
 
+#define PWM_FRQ		((float)F_CPU/8.)
+
 void start_tone(uint8_t idx){
 	if (idx == 0) TCCR0B = 2; // presc=8 -> 1MHz
 	else TCCR1 = 4; // presc=8 -> 1MHz
@@ -34,12 +36,12 @@ void music_init(void){
 
 float tone_on[2];
 uint16_t goal_ovf[2] = {0};
-float tone_slope[2] = {0};
+float K[2] = {0};
 bool tone_flag[2], dir[2];
 
 static void Draw(uint8_t idx, uint16_t start, float slope){
-	tone_on[idx] = start;
-	tone_slope[idx] = slope;
+	tone_on[idx] = PWM_FRQ/start;
+	K[idx] = slope/(PWM_FRQ*PWM_FRQ);
 }
 
 void set_timer(uint8_t idx, uint16_t time){
@@ -61,7 +63,9 @@ inline void tone_isr(uint8_t idx){
 			set_timer(idx, tone_on[idx]);
 			set_pin_to(TONE_PIN[idx], 1);
 			//tone_on[idx] *= 1.001;
-			tone_on[idx] += tone_on[idx]*tone_on[idx]*tone_slope[idx];
+			//tone_on[idx] += tone_on[idx]*tone_on[idx]*tone_slope[idx];
+			float p0 = tone_on[idx];
+			tone_on[idx] = p0/(1. + K[idx]*p0*p0);
 		}
 		tone_flag[idx] = !tone_flag[idx];
 	}
@@ -79,13 +83,13 @@ ISR(TIM1_OVF_vect){
 
 #define RES			3 // pixel-res = RES x RES
 
-#define VERT_TIME	2	// for one pixel
-#define HORZ_TIME	7	// for one pixel
+#define VERT_TIME	3	// for one pixel
+#define HORZ_TIME	9	// for one pixel
 
-#define	MX			400. // bottom
-#define MN			250. // top
+#define	MX			3500. // bottom
+#define MN			2500. // top
 
-float vert_vals[RES] = {MN, (float)2/(1/MX+1/MN), MX};
+#define VERT_STEP	((MX - MN) / (RES - 1.))
 
 // send 0 0 to get a space
 // gets point and calculates period, slope and duration
@@ -106,30 +110,44 @@ void draw_coor(const uint8_t * point, uint16_t * period, float * slope, uint8_t 
 			int8_t e_row = point[1]/RES;
 			if (s_col == e_col) *duration = VERT_TIME;
 			else				*duration = (e_col - s_col) * HORZ_TIME;
-			*period = vert_vals[s_row]; // starting period
-			*slope = (vert_vals[e_row] - vert_vals[s_row]) / (float)*duration / 22000000.;
+			*period = VERT_STEP * (RES - 1 - s_row) + MN; // starting period
+			*slope = (float)(s_row - e_row) * VERT_STEP / (float)*duration * wdt_frq_hz;
 		}
 	}
 }
 
-#define W	0,7, 7,2, 0,7, 7,2
-#define V	0,7, 7,2
-#define L	0,6, 6,8
 #define S	0,0
+#define L	0,6, 6,8
+#define U	0,6, 6,8, 8,2
+#define V	0,7, 7,2
+#define W	0,7, 7,2, 0,7, 7,2
+#define M	6,1, 1,8, 6,1, 1,8
+#define N	6,1, 1,8, 6,1
+//#define J	3,6, 6,8, 8,2
+#define T	0,2, 1,7, 0,2
 #define H	0,6, 3,5, 0,6
+#define I	1,7
 #define Y	0,5, 4,7, 3,2
+#define P2	6,0, 0,2
+#define P6	0,2, 2,8
+#define P7	V
+#define P8	6,1, 1,8
+#define PI	6,0, 0,2, 2,8
+#define LAMBDA	6,5, 5,2, 3,8
+#define DASH	3,4
+#define HYPHEN	6,8
 
-const uint8_t coords[] = {W,S,V,S,L,S,H,S,Y,S,S};
+const uint8_t coords[] = {L,S,U,S,V,S,W,S,M,S,N,S,T,S,H,S,Y,S,P2,S,P6,S,P7,S,P8,S,PI,S,LAMBDA,S,DASH,S,HYPHEN,S,I};
 
 // Possible single speaker:
-// English: LUVNMWJHTY , hij
+// English: LUVNMWJHTYI
 // persian 2 6 7 8
-// greek: ? ? ?
+// greek: pi, lambda
 
 /*
-0 1 2 // MN	 \/
-3 4 5 // MD	 \/
-6 7 8 // MX	 \/
+0 1 2 // MX	 \/
+3 4 5 // 	 \/
+6 7 8 // MN	 \/
 */
 
 struct  
@@ -142,24 +160,25 @@ struct
 
 
 void process_draw_sound_words(void){
-
-	if (state[0].duration == 0){
-		if (state[0].idx == (sizeof(coords)/sizeof(coords[0])))
-		{state[0].idx = 0; state[0].duration = 0;}
-		
-		draw_coor(&coords[state[0].idx], &state[0].period, &state[0].slope, &state[0].duration);
-		
-		if (state[0].period) {
-			Draw(0, state[0].period, state[0].slope);
-			//Draw(0, 300, 200./170000000.);
-			start_tone(0);
-		}
-		else stop_tone(0);
-		
-		state[0].idx += 2;
-	}
-	else state[0].duration--;
 	
+	for (uint8_t i = 0; i < 1; i++)
+	{
+		if (state[i].duration == 0){
+			if (state[i].idx == (sizeof(coords)/sizeof(coords[i])))
+			{state[i].idx = 0; state[i].duration = 0;}
+		
+			draw_coor(&coords[state[i].idx], &state[i].period, &state[i].slope, &state[i].duration);
+		
+			if (state[i].period) {
+				Draw(i, state[i].period, state[i].slope);
+				start_tone(i);
+			}
+			else stop_tone(i);
+		
+			state[i].idx += 2;
+		}
+		else state[i].duration--;
+	}
 	//return; // doesn't work for 2!
 	
 /*
--- Attiny/tiny85/setts.h
+++ Attiny/tiny85/setts.h
@@ -3,7 +3,7 @@
 #define SETTS_H
 
 // set 0 to make it variable wdt period and use function set_wdt_period(wdt_per_ms)
-#define FIXED_WDT_PER	WDT_64ms
+#define FIXED_WDT_PER	WDT_32ms
 
 #define WDT			1
 #define PCINT		0
